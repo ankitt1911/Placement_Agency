5c3b20d3e4e98148bba3e977d69d35a2ed3d31dd
ankitj2w <ankit.yadav@joulestowatts.com>
2026-07-15 20:47:56 +0530

    backend link fixed

--- Frontend/src/Services/apiService.jsx
+++ Frontend/src/Services/apiService.jsx
@@ -4,7 +4,7 @@ import store from "../ReduxStore/store";
 import { logout } from "../ReduxFeature/Authenthicate/LoginSlice";
 
 const ApiRequest = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+  baseURL:"https://placement-agency.onrender.com/api",
   timeout: 20000
 });
 
